b84adac1adf826b0fe69caa5539cb4bb607439c8
Lee <klee232@ad.brown.edu>
2023-04-07 12:01:01 -0400

    2304d

--- Image_Segmentation/optimal_gaborfilt.m
+++ Image_Segmentation/optimal_gaborfilt.m
@@ -33,20 +33,10 @@ function [mag_gabor,phase_gabor]=optimal_gaborfilt(image,factor_wav,factor_angle
                              max_freq_wavelength(2)^2);
     min_freq_wavelength=sqrt(min_freq_wavelength(1)^2 + ...
                              min_freq_wavelength(2)^2);
-    % computing the optimal wavelengthes for gabor filters
-    % distribute the wave factor between the range of common and maximum
-    % wavelength and common and minimum wavelength
-    dist_commax=min_freq_wavelength-common_wavelength;
-    dist_commin=common_wavelength-max_freq_wavelength;
-    fact_commax=dist_commin/(dist_commin+dist_commax)*factor_wav;
-    fact_commax=round(fact_commax);
-    fact_commin=factor_wav-fact_commax;
-    % make up the wave grids 
-    % note that the gabbor filter function requires the wavelengthes to be
-    % always larger or equal to 2
-    grid_commax=max([2,max_freq_wavelength],[],'all'):max([2,common_wavelength],[],'all'):(dist_commax)/fact_commax;
-    grid_commin=max([2,common_wavelength],[],'all'):(dist_commin)/fact_commin:max([2,min_freq_wavelength],[],'all');
-    grid_wavelengths=[grid_commax, grid_commin];
+    % compute optimal wavelengths
+    optimal_waves=optimal_wav_gen(common_wavelength,max_freq_wavelength,min_freq_wavelength);
+
+
 
     % angle computation
     % divide the angle from 0 to 180 into several grids
