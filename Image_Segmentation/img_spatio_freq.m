c534fca982634d28b9ab6f0726e310c31f758f31
Lee <klee232@ad.brown.edu>
2023-04-14 11:58:47 -0400

    added Image Fourier Transform

--- Image_Segmentation/img_spatio_freq.m
+++ Image_Segmentation/img_spatio_freq.m
@@ -19,8 +19,8 @@ function [common_freq, common_ang, ...
     image=p.Results.image;
 
     % find out the dimension of the image
-    wid_img=size(image,1);
-    len_img=size(image,2);
+    nx=size(image,1);
+    ny=size(image,2);
     chn_img=size(image,3);
     % if the image is a color image, convert it to grayscale image
     if chn_img~=1
@@ -31,33 +31,95 @@ function [common_freq, common_ang, ...
     end
 
     % conduct fourier transform on image
-    spatio_freq=fft2(image);
-    spatio_freq=fftshift(spatio_freq);
+%     spatio_freq=fft2(image);
+%     spatio_freq=fftshift(spatio_freq);
     
+    % using the convention in https://www.mathworks.com/help/signal/ug/power-spectral-density-estimates-using-fft.html
+    fs = 1/1.0;  % our x has 1.0 as an interval
+    spatio_freq=fftshift(fft2(image));
+%     spatio_freq=fft2(image);
+    
+%     spatio_freq = spatio_freq(nx/2:nx,:);  % take the area for kx>0
+    spatio_freq = (1/(fs*nx*ny)) * abs(spatio_freq).^2;
+%     spatio_freq(2:end-1) = 2*spatio_freq(2:end-1);
     % show the image frequency plot
-    figure(1);
-    title("Fourier Transform Plot")
-    imagesc(abs(spatio_freq))
+    x=(1:nx);
+    x=x-1;
+    x=x/ny;
+    x=x-0.5;
+    y=(1:ny);
+    y=y-1;
+    y=y/nx;
+    y=y-0.5;
+    kx = 2*pi * x;
+    ky = 2*pi * y;
+    kx = kx((nx/2+1):nx);
+
+%     freq = 0:fs/length(x):fs/2;
+    kx = 0:fs/nx:fs/2;  % when you have the sampling freq. fs, the maximum freq you can inspect with FFT is fs/2.
+    kx = -fs/2:fs/nx:fs/2;
+    kx = kx(2:end);
+    ky = -fs/2:fs/ny:fs/2;
+    ky = ky(2:end);  % ky(1) = -kyMax is omitted in fft
+
+    figure;
+    title("2D Power spectral density")
+    imagesc(kx, ky, spatio_freq')
+    axis image
+    ax = gca;
+    ax.YDir = "normal";
+    xlabel("k_x")
+    ylabel("k_y")
+
 
     % plot the power spectral density
-    spatio_freq_mag=abs(spatio_freq).^2/(wid_img*len_img);
-    wid_rang=(1:wid_img);
-    len_rang=(1:len_img);
-    [wid_mat, len_mat]=ndgrid(wid_rang,len_rang);
-    wid_mat=wid_mat-1; % subtract every element in horizontal direction by 1 
-    wid_mat=wid_mat/len_img; % divide every element in horizontal direction by the length of the image
-    len_mat=len_mat-1; % subtract every element in vertical direction by 1
-    len_mat=len_mat/wid_img; % divide every element in vertical direction by the width of the image
-    % centralize the frequency scale
-    wid_mat=wid_mat-0.5;
-    len_mat=len_mat-0.5;
-    % calculate the overall frequency
-    freqs_x=2*pi*wid_mat;
-    freqs_y=2*pi*len_mat;
-    freqs=sqrt(freqs_x.^2+freqs_y.^2);
-    figure(2);
+    spatio_freq_mag=abs(spatio_freq).^2/(nx*ny);
+%     wid_rang=(1:wid_img);
+%     len_rang=(1:len_img);
+%     [wid_mat, len_mat]=ndgrid(wid_rang,len_rang);
+%     wid_mat=wid_mat-1; % subtract every element in horizontal direction by 1 
+%     wid_mat=wid_mat/len_img; % divide every element in horizontal direction by the length of the image
+%     len_mat=len_mat-1; % subtract every element in vertical direction by 1
+%     len_mat=len_mat/wid_img; % divide every element in vertical direction by the width of the image
+%     % centralize the frequency scale
+%     wid_mat=wid_mat-0.5;
+%     len_mat=len_mat-0.5;
+%     % calculate the overall frequency
+%     freqs_x=2*pi*wid_mat;
+%     freqs_y=2*pi*len_mat;
+%     freqs=sqrt(freqs_x.^2+freqs_y.^2);
+    [gkx, gky] = ndgrid(kx, ky);
+    k = sqrt(gkx.^2 + gky.^2);
+    kSort = k(:);
+    [kSort, is] = sort(kSort);
+    spatio_freq_mag_sort = spatio_freq_mag(:);
+    spatio_freq_mag_sort = spatio_freq_mag_sort(is);
+   
+    figure;
     title("Power Spectral Density Plot")
-    plot(freqs,spatio_freq_mag);
+    plot(kSort, spatio_freq_mag_sort);
+
+    kx1 = kx(end/2+1:end);
+    spatio_freq_mag1 = spatio_freq_mag(:,end/2+1:end);
+    figure;
+    title("2D Fourier Transform Plot for positive kx and ky")
+    imagesc(kx1, ky, abs(spatio_freq_mag1))
+    ax = gca;
+    ax.YDir = 'normal';
+    xlabel("k_x")
+    ylabel("k_y")
+
+
+    max_mag = max(spatio_freq_mag1, [], 'all');
+    [ikyMax, ikxMax] = find(spatio_freq_mag1 == max_mag);
+    kxMax = kx1(ikxMax);
+    kyMax = ky1(ikyMax);
+    kMax = sqrt(kxMax^2 + kyMax^2)
+    angMax = atan(kyMax/kxMax)
+
+
+
+
 
     % find the most common frequency
     max_mag=max(spatio_freq_mag,[],'all');
@@ -69,18 +131,18 @@ function [common_freq, common_ang, ...
     if numel(com_x)>1
         half_com=ceil(numel(com_x)/2);
         com_freq_x=abs(flip(com_x(1:half_com))-com_x(half_com+1:end))/2;
-        com_freq_x=com_freq_x/len_img;
+        com_freq_x=com_freq_x/ny;
         com_freq_y=abs(flip(com_y(1:half_com))-com_y(half_com+1:end))/2;
-        com_freq_y=com_freq_y/wid_img;
+        com_freq_y=com_freq_y/nx;
         % find the most common angle for the frequency    
         re_comp=real(spatio_freq(com_x(half_com+1:end),com_y(half_com+1:end)));
         im_comp=imag(spatio_freq(com_x(half_com+1:end),com_y(half_com+1:end)));
         com_ang=atan(im_comp/re_comp);
     else
-        com_freq_x=com_x(1)-(len_img/2);
-        com_freq_x=com_freq_x/len_img;
-        com_freq_y=com_y(1)-(wid_img/2);
-        com_freq_y=com_freq_y/wid_img;
+        com_freq_x=com_x(1)-(ny/2);
+        com_freq_x=com_freq_x/ny;
+        com_freq_y=com_y(1)-(nx/2);
+        com_freq_y=com_freq_y/nx;
         re_comp=real(spatio_freq(com_x(1),com_y(1)));
         im_comp=imag(spatio_freq(com_x(1),com_y(1)));
         com_ang=atan(im_comp/re_comp);
@@ -92,8 +154,8 @@ function [common_freq, common_ang, ...
 
     % find the maximum frequency
     [max_x,max_y]=find(spatio_freq_mag,1,'last');
-    max_freq_x=(max_x(1)-1)/len_img;
-    max_freq_y=(max_y(1)-1)/wid_img;
+    max_freq_x=(max_x(1)-1)/ny;
+    max_freq_y=(max_y(1)-1)/nx;
     max_freq_x=max_freq_x-0.5;
     max_freq_y=max_freq_y-0.5;
     % calculate the angle for the maximum frequency
@@ -106,8 +168,8 @@ function [common_freq, common_ang, ...
 
     % find the minimum frequency
     [min_x,min_y]=find(spatio_freq_mag,1,'first');
-    min_freq_x=(min_x(1)-1)/len_img;
-    min_freq_y=(min_y(1)-1)/wid_img;
+    min_freq_x=(min_x(1)-1)/ny;
+    min_freq_y=(min_y(1)-1)/nx;
     min_freq_x=min_freq_x-0.5;
     min_freq_y=min_freq_y-0.5;
     % calculate the angle for the maximum frequency
@@ -120,12 +182,12 @@ function [common_freq, common_ang, ...
 
 
     % check if the final frequency is larger than the base frequency: 2*pi/wid_img (need more clarification)
-    thres_x=2*pi/wid_img;
+    thres_x=2*pi/nx;
     if com_freq_x<thres_x
         com_freq_x=0;
     end
     % check if the final frequency is larger than the base frequency: 2*pi/len_img (need more clarification)
-    thres_y=2*pi/len_img;
+    thres_y=2*pi/ny;
     if com_freq_y<thres_y
         com_freq_y=0;
     end
